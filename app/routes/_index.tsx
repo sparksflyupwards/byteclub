a142c066a2a2e31c6c98bc875e0e576e335f97f8
Shakaib Khan <shakaibkhan@Shakaibs-MacBook-Pro.local>
2025-05-20 12:24:40 -0400

    Add openai chat feature

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ import CodeExecutor from "~/components/CodeExecutor";
 import { useLoaderData } from "@remix-run/react";
 import { LanguageOption } from "~/interface/CodeExecutionSchema";
 import DatabaseConnectionService from "~/database/connection/DatabaseConnectionService";
-import QuestionDisplay from "~/components/QuestionDisplay";
+import TabDisplay from "~/components/TabDisplay";
 import '../stylesheets/index.css'
 import ResizableHorizontal from "~/components/ResizableHorizontal";
 
@@ -240,7 +240,7 @@ export default function Index() {
         />
         
         <ResizableHorizontal>
-          <QuestionDisplay
+          <TabDisplay
             question={randomQuestion}
             questionTags={randomQuestionsTags}
             activeTab={activeTab}
